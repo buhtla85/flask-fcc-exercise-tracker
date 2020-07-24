adc7f61877897a4e00491bcaa04550eec95e4f87
buhtla85 <tamara.naunovic@gmail.com>
2020-07-24 19:48:57 +0200

    new defs and routes

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ class Exercise(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(100), nullable=False)
     duration = db.Column(db.Integer, nullable=False)
-    date = db.Column(db.DateTime, default=datetime.utcnow)
+    date = db.Column(db.DateTime(), default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
 # marshmallow schemas
@@ -31,29 +31,42 @@ class UserSchema(ma.Schema):
         fields = ('id', 'name')
 
 #schema for returning {id: "", username: "", date: "", duration: x, description: ""} /api/exercise/add
-class AddExerciseToUser(ma.Schema):
+class AddExerciseToUserSchema(ma.Schema):
     class Meta:
-        fields = ('id', 'name', 'date', 'duration', 'description')
+        fields = ('user_id', 'name', 'date', 'duration', 'description')
+
+#nested "log" schema 
+class LogSchema(ma.Schema):
+    class Meta: 
+        fields = ('description', 'duration', 'date')
 
 #schema for returning {id: "", name:"", count: x, log: [{description: "", duration: x, date: ""}, {...}]}
+class UserAndExercisesSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'name', 'count', 'log')
+        log = ma.List(ma.Nested(LogSchema, many=True))
 
-# init schema
+# init schemas
 user_schema = UserSchema()
 users_schema = UserSchema(many=True)
+add_single_exercise = AddExerciseToUserSchema()
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == 'POST':
+    if request.method == 'POST' and request.form['action'] == 'user':
         return redirect(url_for('new_user'), code=307)
+    elif request.method == 'POST' and request.form['action'] == "exercise":
+        return redirect(url_for('add_exercise'), code=307)
+    else: 
+        return render_template('index.html')
 
-    return render_template('index.html')
-
-
+#add new user
+#this route works
 @app.route('/api/exercise/new-user', methods=['POST'])
 def new_user():
-    new_name = request.form.get('name')
-    if len(new_name) <= 1 and len(new_name) > 20:
+    new_name = request.form['name']
+    if len(new_name) <= 1 or len(new_name) > 20:
         return 'Number of characters must be grater than 1 and less than 20.'
     else:
         new_username = User(name=new_name)
@@ -65,17 +78,42 @@ def new_user():
             return 'There was an issue adding user.'
     
 
-# get all users from db and show them as json (as a list - [{id: mnmn, name: jghgjg}, {id: klkk, name: kjhjhj}, {}]) on this endpoint 
+# get all users from db and return them to the user in json format (list - [{id: mnmn, name: jghgjg}, {id: klkk, name: kjhjhj}, {}])
+# this route works and returns the expected json format 
 @app.route('/api/exercise/users', methods=['GET'])
 def get_all_users():
     data = User.query.all()
     result = users_schema.dump(data)
     return jsonify(result)
-    
 
 
-# upon adding exercise we are being redirected to /api/exercise/add and the added exercise details are displayed in json format 
+# add exercise and return them to the user in json format
+@app.route('/api/exercise/add', methods=['POST'])
+def add_exercise():
+    user_id = request.form['userId']
+    num_user_id = int(float(user_id))
 
+    description = request.form['description']
+    duration = request.form['duration']
+    time = request.form['date']
+    time_old = time.replace('T', ' ')
+    time_new = datetime.strptime(time_old, '%Y-%m-%d')
+
+    num_duration = int(float(duration))
+
+
+    # if len(user_id) == 0 or len(description) == 0 or len(duration) == 0:
+    #     return 'All fields are required.'
+    # elif not verifiy_userID:
+    #     return 'Invalid id.'
+    # else:
+    exercise_data = Exercise(description=description, duration=num_duration, date=time_new, user_id=num_user_id)
+        
+    db.session.add(exercise_data)
+    db.session.commit()
+    return add_single_exercise.jsonify(exercise_data)
+        
+            # return 'There was an issue adding user.'
 
 
 
